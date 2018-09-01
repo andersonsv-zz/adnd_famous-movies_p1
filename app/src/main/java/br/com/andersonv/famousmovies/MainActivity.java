0d6c10e3acddc13ddac1947357046fdeaf82bf3b
Your Name <anderson.si.vieira@hotmail.com>
2018-09-01 20:39:20 -0300

    Remoção de constrait layout e inclusao de nova tela de detalhe

--- app/src/main/java/br/com/andersonv/famousmovies/MainActivity.java
+++ app/src/main/java/br/com/andersonv/famousmovies/MainActivity.java
@@ -3,14 +3,13 @@ package br.com.andersonv.famousmovies;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_movie);
     }
 
     @Override
