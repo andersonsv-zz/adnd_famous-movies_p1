7800340baaa3568a69ea7721a65115d5a090b454
Your Name <anderson.si.vieira@hotmail.com>
2018-09-01 19:46:57 -0300

    Inclusão de menu de contexto

--- app/src/main/java/br/com/andersonv/famousmovies/MainActivity.java
+++ app/src/main/java/br/com/andersonv/famousmovies/MainActivity.java
@@ -2,6 +2,8 @@ package br.com.andersonv.famousmovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_list_movies, menu);
+        return true;
     }
 }
