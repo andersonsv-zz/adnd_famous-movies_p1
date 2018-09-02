0b199bf58f4f24704dcb43ab391675072eaf4c7c
Your Name <anderson.si.vieira@hotmail.com>
2018-09-02 16:01:39 -0300

    Adicionado data json e ajustes no detalhe

---
+++ app/src/main/java/br/com/andersonv/famousmovies/MovieDetailActivity.java
@@ -0,0 +1,14 @@
+package br.com.andersonv.famousmovies;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+
+public class MovieDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_detail);
+    }
+}
